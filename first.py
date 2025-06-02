cd7a14ff5ef1c34efde623f7268b056f341e8507
young <hjy32946029@gmail.com>
2025-06-02 13:00:22 +0900

    Add: Markdown add

--- first.py
+++ first.py
@@ -5,9 +5,14 @@ print ("그리고 파일 이름 자체도 노락색으로 바뀝니다")
 # 숫자 자료형
 100
 
+# Day 1
+## input(), print()
 name = input("young")
 print(f"{name}님, 파이썬에 오신 것을 환영합니다")
 
 name = input("young")
 age = input(21)
 print(f"{name}님은 {age}살입니다")
+
+# Day 2
+## 변수와 자료형
